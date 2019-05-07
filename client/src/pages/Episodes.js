8b00c6372eb913eb159568628c562a05b2d34ff0
Addison Grillo <addisongrillo@gmail.com>
2019-05-07 16:35:19 -0400

    episodes page and a few other tweaks

--- client/src/pages/Episodes.js
+++ client/src/pages/Episodes.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import Navbar from '../components/Navbar'
 import './Episodes.css'
 import axios from 'axios'
-import ls from './pics/ls.png';
+import ls from './pics/ls.png'
+
+
+
 
 
 class Episodes extends Component {
-  state = { episode: [] }
-
+  state = { episode: [''] }
+  
   getEpisode = (e) => {
     e.preventDefault();
     const episode = e.target.value;
@@ -21,32 +24,49 @@ class Episodes extends Component {
       .catch(err => {
         console.log(err)
       })
+      setTimeout(function(){ let element = document.getElementById("epi");
+      element.classList.remove("hidden"); }, 500);
+      
     }else{
       return
     }
   }
-
+ 
 
   render(){
     return(
       <>
         <Navbar/>
-        <div id="lsdiv">
+        <div id="lsediv">
         <h1>Star Wars</h1>
-        <img alt="lightsabers" id="ls" src={ls}></img>
+        <img alt="lightsabers" id="lse" src={ls}></img>
         <h1>Episodes</h1>
         </div>
-        <select id="selector" onChange={this.getEpisode}>
-          <option value="1">A New Hope</option>
-          <option value="2">Empire Strikes Back</option>
-          <option value="3">Return of the Jedi</option>
-          <option value="4">Phantom Menace</option>
-          <option value="5">Attack of the Clones</option>
-          <option value="6">Revenge of the Sith</option>
-          <option value="7">The Force Awakens</option>
-        </select>
-        <div className="episode">
-        <p>Title: {this.state.episode.title}</p>
+        <div id="middle">
+          <select id="selector" placeholder="choose" onChange={this.getEpisode}>
+            <option value="">---</option>
+            <option value="1">A New Hope</option>
+            <option value="2">Empire Strikes Back</option>
+            <option value="3">Return of the Jedi</option>
+            <option value="4">Phantom Menace</option>
+            <option value="5">Attack of the Clones</option>
+            <option value="6">Revenge of the Sith</option>
+            <option value="7">The Force Awakens</option>
+          </select>
+          <div id="epi" className="episode hidden">
+          <div id="poster">
+          <img src={'./posters/' + this.state.episode.episode_id + ".jpg"}/>
+          </div>
+          <div id="results">
+          <p>Title: {this.state.episode.title}</p>
+          <p>Episode ID: {this.state.episode.episode_id}</p>
+          <p>Release Date: {this.state.episode.release_date}</p>
+          <p>Directed By: {this.state.episode.director}</p>
+          <p>Produced By: {this.state.episode.producer}</p>
+          <p>Opening Crawl: {this.state.episode.opening_crawl}</p>
+          
+          </div>
+          </div>
         </div>
       </>
     )
